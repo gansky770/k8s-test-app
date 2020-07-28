d7fc219a206caea0ccf92ca9be4a5e4013aaebff
Moti Gansky <Moti.Gansky@Strauss-Group.com>
2020-07-28 15:46:19 +0300

    check-test

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+import botocore
 import boto3
 from pythonjsonlogger import jsonlogger
 import logging
 import datetime
 import os
 
-#define jsonlogger
 
+
+
+#formatter constructor
 class CustomJsonFormatter(jsonlogger.JsonFormatter):
     def add_fields(self, log_record, record, message_dict):
         super(CustomJsonFormatter, self).add_fields(log_record, record, message_dict)
@@ -16,9 +19,7 @@ class CustomJsonFormatter(jsonlogger.JsonFormatter):
             log_record['timestamp'] = now
         if  not log_record.get('Running clusters'):
             log_record['Running clusters'] = int(len(cluster_info)/2)
-        #if not message_dict:
-    
-         #cluster IP nad name function  TODO 
+         
 
          
 cluster_info={}
@@ -29,35 +30,46 @@ def running_cluster_info_test():
     for instance in running_instances:
         i+=1
         ip=running_instances[instance] #instance.private_ip_address
-        name=instance 
+        name=instance #instace.state['Name']
         cluster_info['Cluster'+str(i)+'_IP']=ip
         cluster_info['Cluster'+str(i)+'_Name']=name   
     
 
+
+#integrate boto3
+
+# Use the filter() method of the instances collection to retrieve
+# all running EC2 instances.
+def running_cluster_info():
+    i=0 
+    ec2 = boto3.resource('ec2')
+    running_instances = ec2.instances.filter(
+    Filters=[{'Name': 'tag:k8s.io/role/master', 'Values': ['1']},
+             {'Name': 'instance-state-code', 'Values': ['16']}])
+    for instance in running_instances:
+        i+=1
+        ip=instance.private_ip_address
+        name=instance.state['Name']
+        cluster_info['Cluster'+str(i)+'_IP']=ip
+        cluster_info['Cluster'+str(i)+'_Name']=name   
+             
+
+             
+
+
+#define formatter for the log messages (base on class CustomJsonFormatter )
 formatter = CustomJsonFormatter('%(threadName)s  - %(name)s - %(timestamp)s  - %(msecs)s - %(Running clusters)s   - %(message)s - %(levelname)s ')
+#define jsonlogger
 logHandler = logging.StreamHandler()
 logHandler.setFormatter(formatter)
 logger = logging.getLogger()
 logger.addHandler(logHandler)
 logger.setLevel(logging.INFO)
-running_cluster_info_test()
-logger.info( 'testing K8S REPORTING',extra=cluster_info,)
+#running_cluster_info_test()
+running_cluster_info()
+logger.info( 'testing K8S REPORTING',extra=cluster_info)
    
 
-
-#integrate boto3
-
-
-# Boto3
-# Use the filter() method of the instances collection to retrieve
-# all running EC2 instances.
-# def cluster_info():
-#     ec2 = boto3.resource('ec2')
-#     running_instances = ec2.instances.filter(
-#     Filters=[{'Name': 'tag:k8s.io/role/master', 'Values': ['1']},
-#              {'Name': 'instance-state-code', 'Values': ['16']}])
-#              for instance in running_instances:
-#              
 
 
 
---
+++ readme.txt
@@ -0,0 +1,2 @@
+1. put the config &cred files in .~/.aws/ folder  (run aws configure >>>its creat  the folder automaticly) ## dir : linux >> ls  ~/.aws  windows: C:\> dir "%UserProfile%\.aws"
+2. insert accesskey to credentials file
